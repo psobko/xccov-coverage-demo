5fb3135ea12476cefe8b7035badf3eed8a21aaeb
peter sobkowksi <peter@psobko.io>
2018-04-22 19:15:45 -0400

    updating test runner script

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 
 #!/bin/bash
+
+#1.Build the project and run tests
 xcodebuild -scheme xccov -derivedDataPath ./Build/ -destination 'platform=iOS Simulator,OS=11.3,name=iPhone 7' -enableCodeCoverage YES clean build test
+
+#2.Generate thte JSON coverage data
+xcrun xccov view --json ./*.xccovreport
